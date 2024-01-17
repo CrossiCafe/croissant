a64b373cd7020167d7819b4b5ac6993188d6b309
DelfinaMolter <delfi.molter@gmail.com>
2024-01-17 19:58:56 -0300

    Menu listo.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Agrega un cargador para archivos PDF
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'static/pdf/', // Ruta donde se guardarán los archivos PDF en la construcción
+          },
+        },
+      ],
+    });
+
+    // Importante: Regresa la configuración modificada
+    return config;
+  },
+}
 
 module.exports = nextConfig
