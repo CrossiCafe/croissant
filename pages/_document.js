238e50a0a13c68d11f457b4add6c0bebfc316ecd
Cristian Lopez <cristian.lopez@xyclon.com.ar>
2024-05-28 14:47:02 -0300

    Se ajusta script google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,23 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Aquí incluye el script de Metricool */}
+          {/* Script de Metricool */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
                 function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"bd2713b53b423865476c61e6b4762b92"})});
+              `,
+            }}
+          />
+          {/* Script de Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-1L01SLY277"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-1L01SLY277');
               `,
             }}
           />
